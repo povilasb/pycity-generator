afb551a6500e22f7f7465cc1288a4307f39732b9
Povilas Balciunas <balciunas90@gmail.com>
2016-04-06 20:52:21 +0300

    Add facility to walk directory recursively

--- src/filesys.py
+++ src/filesys.py
@@ -83,3 +83,75 @@ def get_subdir_parent(dir_tree, subdir_path):
     """
     parent_path, curr_dir = os.path.split(subdir_path)
     return get_subdir_node(dir_tree, parent_path), curr_dir
+
+
+class Node(object):
+    """Filesystem node: file or directory.
+    """
+    def __init__(self, root_path, name):
+        """
+        Args:
+            root_path (str): node location in file system.
+            name (str): file or directory name.
+        """
+        self.root_path = root_path
+        self.name = name
+
+
+class File(Node):
+    """Represents file in filesystem."""
+    pass
+
+
+class Directory(Node):
+    """Represents directory in filesystem."""
+    pass
+
+
+def walk(path, callback, callback_arg=None):
+    """Traverse the directory recursively.
+
+    Invokes the specified callback for every file system node.
+    Passes the specified argument to the callback.
+    The callback might return any value which will be passed to next
+    walk() call as a callback argument.
+
+    Args:
+        path (str): file system path to traverse.
+        callback (callable): function to be called for every traversed node.
+            It's signature is callback(fs_node, arg).
+        callback_arg (any): callback argument. Default is None.
+    """
+    fs_nodes = os.listdir(path)
+    for f in py_files(fs_nodes):
+        callback(File(path, f), callback_arg)
+
+    for d in directories(fs_nodes, path):
+        new_callback_arg = callback(Directory(path, d), callback_arg)
+        walk(os.path.join(path, d), callback, new_callback_arg)
+
+
+def py_files(fs_nodes):
+    """Extracts python files.
+
+    Args:
+        fs_nodes (list): file system nodes.
+
+    Returns:
+        list: nodes whose name ends with '*.py'.
+    """
+    return [f for f in fs_nodes if f.endswith('.py')]
+
+
+def directories(fs_nodes, root_path):
+    """Extracts directories from file system nodes.
+
+    Args:
+        fs_nodes (list): file system nodes.
+        root_path (str): path where nodes are located. It is used to check
+            if the node is a directory.
+
+    Returns:
+        list: nodes whose name ends with '*.py'.
+    """
+    return [f for f in fs_nodes if os.path.isdir(os.path.join(root_path, f))]
--- tests/integration/test_dir_integrations.py
+++ tests/integration/test_dir_integrations.py
@@ -1,8 +1,22 @@
 import os
 
-from hamcrest import assert_that, is_
+from hamcrest import assert_that, is_, instance_of
+from mock import MagicMock, call
 
 from filesys import Dir
+import filesys as fs
+
+
+def assert_called_with_fs_nodes(mock, fs_nodes):
+    arg_i = 0
+    for all_args in mock.call_args_list:
+        args, _ = all_args
+
+        assert_that(args[0], instance_of(type(fs_nodes[arg_i])))
+        assert_that(args[0].name, is_(fs_nodes[arg_i].name))
+
+        arg_i += 1
+
 
 def this_dir():
     """Returns directory path where this file is located.
@@ -39,3 +53,15 @@ def test_py_files_collects_all_python_files_recursively():
             }
         },
     }))
+
+
+def test_walk_collects_all_python_files_recursively():
+    cb = MagicMock()
+
+    fs.walk(fixture_dir('project-with-subdir'), cb)
+
+    assert_that(cb.call_count, is_(4))
+    assert_called_with_fs_nodes(cb, [
+        fs.File('', 'main.py'), fs.Directory('', 'output'),
+        fs.File('', '__init__.py'), fs.File('', 'stdout.py')
+    ])
