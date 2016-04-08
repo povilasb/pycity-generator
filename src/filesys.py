dba9a10f4a6b4fc875bfe11711f9046b7f81b18c
Povilas Balciunas <balciunas90@gmail.com>
2016-04-08 09:09:12 +0300

    Remove unused code

--- src/filesys.py
+++ src/filesys.py
@@ -1,88 +1,9 @@
 import os
 
-class Dir:
-    def __init__(self, path):
-        self.path = path
-
-    def py_files(self):
-        """Collects python files from directory.
-
-        Returns:
-            dict: directory tree with python files collected only.
-        """
-        tree = make_dir_node()
-
-        for root, dirs, dir_files in os.walk(self.path):
-            cur_dir_node = make_dir_node()
-            cur_dir_node['files'] = [f for f in dir_files if f.endswith('.py')]
-
-            parent_dir_node, cur_dir = get_subdir_parent(tree, self.subdir(root))
-            if cur_dir:
-                parent_dir_node['dirs'][cur_dir] = cur_dir_node
-            else:
-                tree['files'] = cur_dir_node['files']
-
-        return tree
-
-    def full_path(self, fname):
-        return os.path.join(self.path, fname)
-
-    def subdir(self, dir_path):
-        """Extracts relative subdirectory path.
-
-        Args:
-            dir_path (str): subdirectory absolute path.
-
-        Returns:
-            str: path to the specified directory relative from this
-                directory.
-            None: if the specified directory is not inside this directory.
-        """
-        if dir_path.startswith(self.path):
-            return dir_path[(len(self.path) + 1):]
 
 def read_file(fname):
     with open(fname, 'r') as f:
         return f.read()
-
-def make_dir_node():
-    """Constructs directory node representing object.
-    """
-    return {
-        'files': [],
-        'dirs': {},
-    }
-
-def get_subdir_node(dir_tree, subdir_path):
-    """
-    Args:
-        dir_tree (dict): root directory to search node for.
-        subdir_path (str): path to subdirectory node to find.
-
-    Returns:
-        dict: directory tree node to the specified subdirectory.
-    """
-    node = dir_tree
-
-    if subdir_path:
-        dirs = subdir_path.split('/')
-        for d in dirs:
-            node = node['dirs'][d]
-
-    return node
-
-def get_subdir_parent(dir_tree, subdir_path):
-    """
-    Args:
-        dir_tree (dict): root directory to search node for.
-        subdir_path (str): path to subdirectory node whose parent we are
-            looking for.
-
-    Returns:
-        dict: directory tree node to the specified subdirectory parent node.
-    """
-    parent_path, curr_dir = os.path.split(subdir_path)
-    return get_subdir_node(dir_tree, parent_path), curr_dir
 
 
 class Node(object):
--- tests/integration/test_dir_integrations.py
+++ tests/integration/test_dir_integrations.py
@@ -3,7 +3,6 @@ import os
 from hamcrest import assert_that, is_, instance_of
 from mock import MagicMock, call
 
-from filesys import Dir
 import filesys as fs
 
 
@@ -31,28 +30,6 @@ def fixture_dir(fixture_name):
         str: path to python project fixture.
     """
     return '%s/fixtures/%s' % (this_dir(), fixture_name)
-
-def test_py_files_collects_all_python_source_files_from_specified_directory():
-    test_dir = Dir(fixture_dir('project1'))
-
-    tree = test_dir.py_files()
-
-    assert_that(tree['files'], is_(['main.py', 'out.py']))
-
-def test_py_files_collects_all_python_files_recursively():
-    test_dir = Dir(fixture_dir('project-with-subdir'))
-
-    tree = test_dir.py_files()
-
-    assert_that(tree, is_({
-        'files': ['main.py'],
-        'dirs': {
-            'output': {
-                'files': ['__init__.py', 'stdout.py'],
-                'dirs': {},
-            }
-        },
-    }))
 
 
 def test_walk_collects_all_python_files_recursively():
